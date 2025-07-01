80c84261318134e346dcaa0c3dad8334fc18ad4b
Shweta chavan <shweta@platform9.com>
2025-07-01 23:52:00 +0530

    slack integration

--- go.mod
+++ go.mod
@@ -5,11 +5,13 @@ go 1.24.1
 require (
 	github.com/gophercloud/gophercloud/v2 v2.7.0
 	github.com/robfig/cron/v3 v3.0.1
+	github.com/slack-go/slack v0.17.2
 	github.com/spf13/cobra v1.9.1
 	go.uber.org/zap v1.27.0
 )
 
 require (
+	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
--- pkg/vmpoll/awake.go
+++ pkg/vmpoll/awake.go
@@ -2,12 +2,13 @@ package vmpoll
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/platform9/pcd-vm-saver/pkg/openstack"
 	"go.uber.org/zap"
 )
 
-func AutoAwakeVM() {
+func AutoAwakeVM() (string, error) {
 	zap.S().Infof("Triggering auto awake VMs")
 	ctx := context.TODO()
 
@@ -17,5 +18,14 @@ func AutoAwakeVM() {
 	// Awake by SleepMode UnShelve or Resume
 	openstack.AwakeVMs(ctx, awakeVms)
 
-	// TOOD: Integrate list of VMs awakened with Slack notification
+	// Build success message with list of awakened VMs
+	successMsg := "AutoAwakeVM task completed successfully\n\n"
+	successMsg += "List of VMs awakened:\n"
+
+	for _, vm := range awakeVms {
+		vmStatus := openstack.GetVMStatus(ctx, vm.ID)
+		successMsg += fmt.Sprintf("VM %s (ID: %s) - Current state: %s\n", vm.Name, vm.ID, vmStatus.Status)
+	}
+
+	return successMsg, nil
 }
